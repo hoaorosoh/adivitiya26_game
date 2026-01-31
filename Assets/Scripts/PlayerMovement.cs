0687b2c6e07bbe1878f47120cca3aae668de888d
clear <191123870+AnkitnMittal@users.noreply.github.com>
2026-01-31 23:49:22 +0530

    working character movement & sprite flipping

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,34 @@
+using UnityEditor.Tilemaps;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5;
+    public int facingDirection = 1;
+
+    public Rigidbody2D rb;
+    public Animator anim;
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        if ((horizontal > 0 && transform.localScale.x < 0) || (horizontal < 0 && transform.localScale.x > 0))
+        {
+            Flip();
+        }
+
+        anim.SetFloat("horizontal", Mathf.Abs(horizontal));
+        anim.SetFloat("vertical", Mathf.Abs(vertical));
+
+        Vector2 movement = new Vector2(horizontal, vertical);
+        rb.linearVelocity = movement * speed;
+    }
+
+    void Flip()
+    {
+        facingDirection *= -1;
+        transform.localScale = new Vector3(transform.localScale.x * -1, transform.localScale.y, transform.localScale.z);
+    }
+}
