a0fd99fdb3fde7c1e420c12362339c95b80831b7
Aditya Kumar Verma <adityaverma10328@gmail.com>
2026-02-01 17:02:55 +0530

    something somrthing enemy ai

--- Assets/Scripts/NightBorne.cs
+++ Assets/Scripts/NightBorne.cs
@@ -2,15 +2,52 @@ using UnityEngine;
 
 public class NightBorne : MonoBehaviour
 {
-    Animation anim;
+    Animator anim;
 
     private void Start()
     {
-        anim = GetComponent<Animation>();
+        anim = GetComponent<Animator>();
     }
 
     private void Update()
     {
-        
+        AnimationDebug();
     }
+
+    void AnimationDebug()
+    {
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            anim.SetBool("IsRunning", true);
+        }
+        else
+        {
+            anim.SetBool("IsRunning", false);
+        }
+        if (Input.GetKey(KeyCode.Alpha2))
+        {
+            anim.SetBool("IsAttacking", true);
+        }
+        else
+        {
+            anim.SetBool("IsAttacking", false);
+        }
+        if (Input.GetKey(KeyCode.Alpha3))
+        {
+            anim.SetBool("IsHurt", true);
+        }
+        else
+        {
+            anim.SetBool("IsHurt", false);
+        }
+        if (Input.GetKey(KeyCode.Alpha4))
+        {
+            anim.SetBool("IsDead", true);
+        }
+        else
+        {
+            anim.SetBool("IsDead", false);
+        }
+    }
+
 }
