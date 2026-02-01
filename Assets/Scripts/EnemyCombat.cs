a7bad68599aeee809e1e2131fe5057598b66df1a
clear <191123870+AnkitnMittal@users.noreply.github.com>
2026-02-01 14:34:47 +0530

    implement playerHealth & enemyDamage

---
+++ Assets/Scripts/EnemyCombat.cs
@@ -0,0 +1,12 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class EnemyCombat : MonoBehaviour
+{
+    public int damage = 1;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        collision.gameObject.GetComponent<PlayerHealth>().ChangeHealth(-damage);
+    }
+}
