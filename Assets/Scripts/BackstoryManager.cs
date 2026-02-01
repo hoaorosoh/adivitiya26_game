1894d13218a9f5bb23d3c10d848f2bfedb8576db
Aditya Kumar Verma <adityaverma10328@gmail.com>
2026-02-02 04:49:12 +0530

    made game over scene

--- Assets/Scripts/BackstoryManager.cs
+++ Assets/Scripts/BackstoryManager.cs
@@ -3,13 +3,14 @@ using UnityEngine;
 public class BackstoryManager : MonoBehaviour
 {
     public float waitTime = 20.0f;
+    public string meow = "MainScene";
 
     // Update is called once per frame
     void Update()
     {
         if(waitTime < 0)
         {
-            Utils.SwitchScene("MainScene");
+            Utils.SwitchScene(meow);
         }
         waitTime -= Time.deltaTime;
     }
