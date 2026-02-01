4798339b91785e4c84ea1d979444b3e9963f1c1b
clear <191123870+AnkitnMittal@users.noreply.github.com>
2026-02-01 18:38:53 +0530

    Health UI

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -1,13 +1,23 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int currentHealth;
     public int maxHealth;
+    public Slider slider;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        slider.maxValue = maxHealth;
+        slider.value = currentHealth;
+    }
 
     public void ChangeHealth(int amount)
     {
         currentHealth += amount;
+        slider.value = currentHealth;
 
         if (currentHealth <= 0)
         {
